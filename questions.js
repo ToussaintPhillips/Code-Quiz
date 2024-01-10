3d53cbae56cdd34518d261718b65b4fd6e78f3ea
Toussaint Phillips <toussaintphillips@gmail.com>
2024-01-10 20:02:46 +0000

    Added questions for quiz

---
+++ questions.js
@@ -0,0 +1,23 @@
+// questions.js
+
+const questions = [
+    {
+      question: "What does `typeof` operator return when checking the type of `null`?",
+      options: ["Number", "String", "Object", "Undefined"],
+      answer: "Object"
+    },
+    {
+      question: "How do you declare a variable in JavaScript?",
+      options: ["declare", "v", "var, let, const", "define"],
+      answer: "var, let, const"
+    },
+    {
+      question: "What will be logged? `console.log(5 + '5');`",
+      options: ["10", "55", "5", "Error"],
+      answer: "55"
+    }
+  ];
+  
+  
+  
+  
